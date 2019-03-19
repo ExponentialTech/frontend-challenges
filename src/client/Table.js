c991a19c10c84c84c0255539c08e5d1cb65b5c0e
Patrick <patrick@247wallst.com>
2019-03-19 08:43:38 -0400

    Update Table.js Component
    Add DataBar component as a render callback on the ESG Score MaterialTable column

--- src/client/Table.js
+++ src/client/Table.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MaterialTable from 'material-table';
+import DataBar from './DataBar.js';
 
 export default class Table extends Component {
 
@@ -17,7 +18,18 @@ export default class Table extends Component {
                     { title: 'Rev Adj', field: 'CO2 Scope 1 & 2 Revenue Adjusted' },
                     { title: 'Total', field: 'CO2 Scope 3 Adjusted' },
                     { title: 'Rev Adj', field: 'CO2 Scope 3 Revenue Adjusted' },
-                    { title: 'ESG Score', field: 'ESG Score'},
+                    { title: 'ESG Score', field: 'ESG Score', 
+                        cellStyle : data => {
+                            return {
+                                width: "14em",
+                                position: "relative",
+                            }
+                        },
+                        //Bar Chart for Esg Scores
+                        render: rowData => (
+                            <DataBar {...rowData} />
+                        )
+                    },
                 ]}
                 data={this.props.data}
                 title="Impact Assessment Data"
