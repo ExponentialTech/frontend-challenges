278564ee5994b6c4981df248fbb7b519f0cf3024
Ruben Profit <rubenprofit@ac.c4q.nyc>
2019-05-15 19:10:19 -0400

    init commit

---
+++ iad-table/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">Impact Assessment Data</header>
+    </div>
+  );
+}
+
+export default App;
